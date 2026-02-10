376467632b101e97723e2e1c12564c4a15ceefdb
giuliacardinalefoto00-png <giuliacardinalefoto00@gmail.com>
2026-02-10 12:49:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,10 +27,13 @@ initDrops();
 window.addEventListener("resize", initDrops);
 
 function draw() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.02)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "#0F0";
+    ctx.fillStyle = "#00FFFF";
+    ctx.shadowColor = ctx.fillStyle;
+ctx.shadowBlur = 10;
+
     ctx.font = fontSize + "px monospace";
 
     for (let i = 0; i < drops.length; i++) {
